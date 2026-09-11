f09d00aaa3516b29a50d5099007045422e287f64
IEVN1003-11000494 <carlosguerreroutl@gmail.com>
2026-09-10 21:03:47 -0600

    Heroes 10_09_2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('introAngular');
+  title:string='Carlos Alberto Guerrero Garcia'
+  duplicarnumero(a:number):number{
+    return a*2
+  }
+  pelicula={
+    titulo: "Spiderman",
+    fechaLanzamiento: new Date(),
+    precio: 2353
+  }
 }
