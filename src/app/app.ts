c591f6efc635b20d76d7133687a88bdd0f97f327
IEVN1003-11000494 <carlosguerreroutl@gmail.com>
2026-09-11 18:53:25 -0600

    Forms module 11_09_2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,10 @@ import { Component, signal } from '@angular/core';
 
 
 @Component({
- 
+ template:`<div>
+    <h1>{{title}}</h1>
+  <app-heroes-list></app-heroes-list>
+</div>`,
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
